043e3ed0bd960b0f06844311cd6c1bced06388ff
Matthew Agius <mtagius@gmail.com>
2018-10-30 14:28:20 -0400

    added an offset and font

--- sketch.js
+++ sketch.js
@@ -1,8 +1,22 @@
 var calender = [];
-            
+var spacing;
+var xOffset;
+var yOffset;
+var calenderFont;
+
+
+function preload() {
+  calenderFont = loadFont('ChaparralPro-Bold.otf');
+}   
 
 function setup() {
   createCanvas(windowWidth,windowHeight);
+
+  spacing = Math.round((windowWidth * .75) / 30);
+  xOffset = Math.round((windowWidth - (spacing * 30)) / 2)  + Math.round(Math.round(spacing * 2.5) / 2);
+  yOffset = Math.round((windowHeight - (spacing * 11)) / 2);
+
+  console.log("Spacing: " + spacing)
 
   if(localStorage.getItem('calender') != null) {
     calender = JSON.parse(localStorage.getItem('calender'));
@@ -42,24 +56,29 @@ function draw() {
 
   fill('#494926');
 
+  var months = ['jan','feb', 'mar', 'apr', 'may','jun','jul','aug','sep', 'oct', 'nov','dec'];
+  textFont(calenderFont);
+
   for(var i = 0; i < calender.length; i++) {
     for(var j = 0; j < calender[i].length; j++) {
 
       if(calender[i][j] == 1) {
-        ellipse((j * 20) + 20, (i * 20) + 20, 15, 15);
+        ellipse((j * spacing) + xOffset, (i * spacing) + yOffset, Math.round(spacing * .75), Math.round(spacing * .75));
       } else {
 
-        let dist = pythag((j * 20) + 20, (i * 20) + 20);
+        let dist = pythag((j * spacing) + xOffset, (i * spacing) + yOffset);
 
         
-        let size = 2 + (20 * (20 / dist));
-        if (size > 10) size = 10;
+        let size = 2 + (spacing * (spacing / dist));
+        if (dist <= (spacing / 2)) size = Math.round(spacing / 2);
 
-        if(dist > 10) size = 2;
+        if(dist > (spacing / 2)) size = Math.round(spacing / 10);
 
-        ellipse((j * 20) + 20, (i * 20) + 20, size, size);
+        ellipse((j * spacing) + xOffset, (i * spacing) + yOffset, size, size);
       }
     }
+    textSize(spacing - 5);
+    text(months[i], xOffset - Math.round(spacing * 2.5), (i * spacing) + yOffset + (Math.round(spacing / 5)));
   }
 }
 
@@ -79,17 +98,27 @@ function pythag(ellipseX, ellipseY) {
 
 function mousePressed() {
   x = mouseX;
-  x -= 20;
-  x = Math.round(x / 20);
+  x -= xOffset;
+  x = Math.round(x / spacing);
 
   y = mouseY;
-  y -= 20;
-  y = Math.round(y / 20);
+  y -= yOffset;
+  y = Math.round(y / spacing);
 
-  print(mouseY, ", " + mouseX)
-  print((mouseY - 20) / 20 + ", " + (mouseX - 20) / 20)
+  print(mouseY +  ", " + mouseX)
   print(y + ", " + x)
 
-  swapDay(y, x);
+  if(x >= 0 && y >= 0 && y < calender.length && x < calender[y].length) {
+    swapDay(y, x);
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  spacing = Math.round((windowWidth * .75) / 30);
+  xOffset = Math.round((windowWidth - (spacing * 30)) / 2)  + Math.round(Math.round(spacing * 2.5) / 2);
+  yOffset = Math.round((windowHeight - (spacing * 11)) / 2);
+
+  console.log("Spacing: " + spacing)
 }
 
